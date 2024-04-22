7b9681b35d9988dcf3782fd1df438db2029990ec
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-22 11:44:12 +0200

    change payment

--- components/paymentFrame.vue
+++ components/paymentFrame.vue
@@ -16,13 +16,17 @@ const { scriptTag } = useScriptTag(
       isEpayLoaded.value = true
       const paymentwindow = new PaymentWindow({
         'merchantnumber': useRuntimeConfig().public.MerchantNumber,
-        'amount': "10095",
+        'amount': "500",
+        'language': 1,
         'currency': "DKK",
-        'windowstate': "4",
+        'subscription': "1",
+        'subscriptiontype': "recurring",
+        'subscriptionname': 'My sub',
+        'windowstate': "2",
         'paymentcollection': "1",
-        'iframeheight': "600",
-        'iframewidth': "600",
-        'accepturl': "https://epay.rsmsp.dk"
+        'instantcapture': 1,
+        'ownreceipt': 1,
+        'accepturl': "https://epay.rsmsp.dk/?23"
       });
       paymentwindow.append('paymentFrame');
       paymentwindow.open();
