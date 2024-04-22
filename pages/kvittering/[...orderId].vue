7d1314ce6c67671206db1bb0f45e2035c8384aaa
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-22 12:55:55 +0200

    update

---
+++ pages/kvittering/[...orderId].vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  WebOrder : {{ orderId }}
+</div>
+</template>
+
+<script setup lang="ts">
+import type {RouteLocationNormalizedLoaded} from "vue-router";
+
+const route: RouteLocationNormalizedLoaded = useRoute()
+const orderId = route.query.orderId
+</script>
+
+<style scoped>
+
+</style>
