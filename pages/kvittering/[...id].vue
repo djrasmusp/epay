c2bb52d137bf8e79096417ddf0fc7e92453abcdf
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-23 13:09:51 +0200

    wip

--- pages/kvittering/[...id].vue
+++ pages/kvittering/[...id].vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <h2>Kvittering</h2>
-    <strong>Weborder</strong>: {{ route.params.id[0] }}
+    <strong>Weborder</strong>: {{ orderId }}<br>
+    <nuxt-link to="/">Tilbage til epay</nuxt-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import type {RouteLocationNormalizedLoaded} from "vue-router";
 
-const route: RouteLocationNormalizedLoaded = useRoute()
+const orderId: RouteLocationNormalizedLoaded = useRoute().params.id[0]
 </script>
 
 <style scoped>
