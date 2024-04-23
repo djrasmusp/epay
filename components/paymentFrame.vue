24e47c7ea07621c8b5ebdf171b9858092f1df206
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-23 10:46:50 +0200

    wip

--- components/paymentFrame.vue
+++ components/paymentFrame.vue
@@ -1,7 +1,8 @@
 <template>
 <div >
-  {{ hashOrder() }}
-  {{ useRuntimeConfig().public.md5key }}
+  <pre>{{ $route.query.hash }}</pre>
+  <pre>{{ hashValidator(returnQueries) }}</pre>
+  <pre>{{ returnQueries }}</pre>
 <div ref="paymentFrame" id="paymentFrame"/>
 </div>
 </template>
@@ -13,60 +14,57 @@ import { Md5 } from "ts-md5";
 const paymentFrame = ref();
 const orderId = Math.floor(Math.random() * 99999999)
 
+const returnQueries = useRoute().query
+
+const paymentParameter = {
+  'merchantnumber': useRuntimeConfig().public.merchantNumber,
+  'amount': 10000,
+  'language': 1,
+  'currency': "DKK",
+  'subscription': 1,
+  'windowstate': 2,
+  'paymentcollection': 1,
+  'lockpaymentcollection': 1,
+  'instantcapture': 1,
+  'ownreceipt': 1,
+  'accepturl': useRuntimeConfig().public.siteUrl,
+  'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + orderId,
+}
+
+paymentParameter.hash = hashOrder(paymentParameter)
+
 useScriptTag(
     '//ssl.ditonlinebetalingssystem.dk/integration/ewindow/paymentwindow.js',
 
     () => {
-      const paymentwindow = new PaymentWindow({
-        'merchantnumber': useRuntimeConfig().public.merchantNumber,
-        'amount': 500,
-        'language': 1,
-        'currency': "DKK",
-        'subscription': 1,
-        'subscriptiontype': "recurring",
-        'subscriptionname': 'My sub',
-        'windowstate': 2,
-        'paymentcollection': 1,
-        'lockpaymentcollection': 1,
-        'instantcapture': 1,
-        'ownreceipt': 1,
-        //'accepturl': useRuntimeConfig().public.siteUrl + "/api/createOrder",
-        'accepturl': useRuntimeConfig().public.siteUrl,
-        'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + orderId,
-        'hash': hashOrder(),
-      })
+      const paymentwindow = new PaymentWindow(paymentParameter)
 
       paymentwindow.append('paymentFrame');
       paymentwindow.open();
     }
 )
 
-function hashOrder() : string {
-
-  const paymentWindowRequest = {
-    'merchantnumber': useRuntimeConfig().public.merchantNumber,
-    'amount': 500,
-    'language': 1,
-    'currency': "DKK",
-    'subscription': 1,
-    'subscriptiontype': "recurring",
-    'subscriptionname': 'My sub',
-    'windowstate': 2,
-    'paymentcollection': 1,
-    'lockpaymentcollection': 1,
-    'instantcapture': 1,
-    'ownreceipt': 1,
-    'accepturl': useRuntimeConfig().public.siteUrl,
-    'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + orderId,
-  };
+function hashOrder(parameters) : string {
 
   let hashString = "";
-  for (const [key, value] of Object.entries(paymentWindowRequest)) {
-    console.log(value)
+  for (const [key, value] of Object.entries(parameters)) {
     hashString += value
   }
 
-  console.log(hashString)
+
+  return Md5.hashStr(hashString + useRuntimeConfig().public.md5key)
+}
+
+function hashValidator(parameters : object) : string {
+  if(parameters?.hash){
+    delete parameters.hash
+  }
+
+  let hashString = "";
+  for (const [key, value] of Object.entries(parameters)) {
+    hashString += value
+  }
+
 
   return Md5.hashStr(hashString + useRuntimeConfig().public.md5key)
 }
---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+<nuxt-link to="/">TEst</nuxt-link>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
