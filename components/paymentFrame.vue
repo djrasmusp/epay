4508e05e30011b62f7ce58449748986cd920cd6d
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-23 10:05:11 +0200

    wip

--- components/paymentFrame.vue
+++ components/paymentFrame.vue
@@ -1,13 +1,17 @@
 <template>
 <div >
+  {{ hashOrder() }}
+  {{ useRuntimeConfig().public.md5key }}
 <div ref="paymentFrame" id="paymentFrame"/>
 </div>
 </template>
 
 <script setup lang="ts">
 import {useScriptTag} from "@vueuse/core";
+import { Md5 } from "ts-md5";
 
 const paymentFrame = ref();
+const orderId = Math.floor(Math.random() * 99999999)
 
 useScriptTag(
     '//ssl.ditonlinebetalingssystem.dk/integration/ewindow/paymentwindow.js',
@@ -15,28 +19,58 @@ useScriptTag(
     () => {
       const paymentwindow = new PaymentWindow({
         'merchantnumber': useRuntimeConfig().public.merchantNumber,
-        'amount': "500",
+        'amount': 500,
         'language': 1,
         'currency': "DKK",
         'subscription': 1,
         'subscriptiontype': "recurring",
         'subscriptionname': 'My sub',
         'windowstate': 2,
-        'backgroundcolor:': 'A7A7A7',
         'paymentcollection': 1,
         'lockpaymentcollection': 1,
         'instantcapture': 1,
         'ownreceipt': 1,
-        'hash': 'abc',
         //'accepturl': useRuntimeConfig().public.siteUrl + "/api/createOrder",
         'accepturl': useRuntimeConfig().public.siteUrl,
-        'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + crypto.randomUUID(),
+        'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + 1234,
+        'hash': hashOrder(),
       })
 
       paymentwindow.append('paymentFrame');
       paymentwindow.open();
     }
 )
+
+function hashOrder() : string {
+
+  const paymentWindowRequest = {
+    'merchantnumber': useRuntimeConfig().public.merchantNumber,
+    'amount': 500,
+    'language': 1,
+    'currency': "DKK",
+    'subscription': 1,
+    'subscriptiontype': "recurring",
+    'subscriptionname': 'My sub',
+    'windowstate': 2,
+    'paymentcollection': 1,
+    'lockpaymentcollection': 1,
+    'instantcapture': 1,
+    'ownreceipt': 1,
+    'accepturl': useRuntimeConfig().public.siteUrl,
+    'mobilecssurl': useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + 1234,
+  };
+
+  let hashString = "";
+  for (const [key, value] of Object.entries(paymentWindowRequest)) {
+    console.log(value)
+    hashString += value
+  }
+
+  console.log(hashString)
+
+  return Md5.hashStr(hashString + useRuntimeConfig().public.md5key)
+}
+
 </script>
 
 <style scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     public : {
       merchantNumber: process.env.MERCHANT_NUMBER,
       siteUrl: process.env.SITE_URL,
+      md5key: process.env.MD5_KEY
     }
   }
 })
