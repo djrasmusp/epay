937f1043fc703d35ffcc615ae66ca4114177d828
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2024-04-24 12:57:40 +0200

    wip

--- components/paymentFrame.vue
+++ components/paymentFrame.vue
@@ -22,7 +22,7 @@ const paymentParameter : Record<string, string | number> = {
   lockpaymentcollection: 1,
   instantcapture: 1,
   ownreceipt: 1,
-  accepturl: useRuntimeConfig().public.siteUrl + '/api/createOrder',
+  accepturl: useRuntimeConfig().public.siteUrl + '/kvittering',
   mobilecssurl: useRuntimeConfig().public.siteUrl + "/mobile.css?v=" + orderId,
 }
 
